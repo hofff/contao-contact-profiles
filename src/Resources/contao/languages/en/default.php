b90908158d59cac8936df7247c0287b855e30576
David Molineus <david.molineus@netzmacht.de>
2018-11-14 13:30:24 +0100

    Add fields for module configuration, work on translation.

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['CTE']['hofff_contact_profile'][0]  = 'Contact profiles';
+$GLOBALS['TL_LANG']['CTE']['hofff_contact_profile'][1]  = 'Show contact profiles';
--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -2,5 +2,7 @@
 
 declare(strict_types=1);
 
-$GLOBALS['TL_LANG']['MOD']['hofff_contact_profiles'][0] = 'Contact Profiles';
-$GLOBALS['TL_LANG']['MOD']['hofff_contact_profiles'][1] = 'Manage Contact Profiles';
+$GLOBALS['TL_LANG']['MOD']['hofff_contact_profiles'][0] = 'Contact profiles';
+$GLOBALS['TL_LANG']['MOD']['hofff_contact_profiles'][1] = 'Manage contact profiles';
+$GLOBALS['TL_LANG']['FMD']['hofff_contact_profile'][0]  = 'Contact profiles';
+$GLOBALS['TL_LANG']['FMD']['hofff_contact_profile'][1]  = 'Show contact profiles';
