6e44602f835afdb74352912896dfc2be268668f2
David Molineus <david.molineus@netzmacht.de>
2021-06-14 11:49:16 +0200

    Implement preview url listeners

---
+++ src/EventListener/PreviewUrlConvertListener.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hofff\Contao\ContactProfiles\EventListener;
+
+use Contao\CoreBundle\Event\PreviewUrlConvertEvent;
+use Contao\CoreBundle\Framework\Adapter;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\PageModel;
+use Doctrine\DBAL\Connection;
+use Hofff\Contao\ContactProfiles\Model\ContactProfileRepository;
+use PDO;
+use Symfony\Component\HttpFoundation\Request;
+
+final class PreviewUrlConvertListener
+{
+    /** @var ContaoFramework */
+    private $framework;
+
+    /** @var ContactProfileRepository */
+    private $contactProfiles;
+
+    /** @var Connection */
+    private $connection;
+
+    public function __construct(
+        ContaoFramework $framework,
+        ContactProfileRepository $contactProfiles,
+        Connection $connection
+    ) {
+        $this->framework       = $framework;
+        $this->contactProfiles = $contactProfiles;
+        $this->connection      = $connection;
+    }
+
+    /**
+     * Adds the front end preview URL to the event.
+     */
+    public function __invoke(PreviewUrlConvertEvent $event): void
+    {
+        if (!$this->framework->isInitialized()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        if ($request === null) {
+            return;
+        }
+
+        $contactProfile = $this->getContactProfile($request);
+        if ($contactProfile === null) {
+            return;
+        }
+
+        $detailPage = $this->getDetailPage($contactProfile);
+        if ($detailPage === null) {
+            return;
+        }
+
+        $event->setUrl($detailPage->getPreviewUrl('/' . $contactProfile['alias'] ?: $contactProfile['id']));
+    }
+
+    private function getContactProfile(Request $request): ?array
+    {
+        if (!$request->query->has('hofff_contact_profile')) {
+            return null;
+        }
+
+        return $this->contactProfiles->fetchById($request->query->get('hofff_contact_profile'));
+    }
+
+    private function getDetailPage(array $contactProfile): ?PageModel
+    {
+        /** @var Adapter<PageModel> $adapter */
+        $adapter = $this->framework->getAdapter(PageModel::class);
+
+        if ($contactProfile['jumpTo'] > 0) {
+            return $adapter->findByPk($contactProfile['jumpTo']);
+        }
+
+        $statement = $this->connection->executeQuery(
+            'SELECT jumpTo from tl_contact_category WHERE id = :categoryId LIMIT 0,1',
+            ['categoryId' => $contactProfile['pid']]
+        );
+
+        $pageId = $statement->fetch(PDO::FETCH_COLUMN);
+        if ($pageId === false || $pageId < 1) {
+            return null;
+        }
+
+        return $adapter->findByPk($pageId);
+    }
+}
---
+++ src/EventListener/PreviewUrlCreateListener.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hofff\Contao\ContactProfiles\EventListener;
+
+use Contao\CoreBundle\Event\PreviewUrlCreateEvent;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Hofff\Contao\ContactProfiles\Model\ContactProfileRepository;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class PreviewUrlCreateListener
+{
+    /** @var RequestStack */
+    private $requestStack;
+
+    /** @var ContaoFramework */
+    private $framework;
+
+    /** @var ContactProfileRepository */
+    private $contactProfiles;
+
+    public function __construct(
+        RequestStack $requestStack,
+        ContaoFramework $framework,
+        ContactProfileRepository $repository
+    ) {
+        $this->requestStack    = $requestStack;
+        $this->framework       = $framework;
+        $this->contactProfiles = $repository;
+    }
+
+    /**
+     * Adds the news ID to the front end preview URL.
+     *
+     * @throws RuntimeException
+     */
+    public function __invoke(PreviewUrlCreateEvent $event): void
+    {
+        if (!$this->framework->isInitialized() || 'hofff_contact_profiles' !== $event->getKey()) {
+            return;
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+        if ($request === null) {
+            throw new RuntimeException('The request stack did not contain a request');
+        }
+
+        // Return on the contact profile list page
+        if ($request->query->get('table') === 'tl_contact_profile' && !$request->query->has('act')) {
+            return;
+        }
+
+        $contactProfile = $this->contactProfiles->fetchById($this->getId($event, $request));
+        if ($contactProfile === null) {
+            return;
+        }
+
+        $event->setQuery('hofff_contact_profile=' . $contactProfile['id']);
+    }
+
+    /**
+     * @return int|string
+     */
+    private function getId(PreviewUrlCreateEvent $event, Request $request)
+    {
+        // Overwrite the ID if the contact profile settings are edited
+        if ('tl_contact_profile' === $request->query->get('table') && 'edit' === $request->query->get('act')) {
+            return $request->query->get('id');
+        }
+
+        return $event->getId();
+    }
+}
