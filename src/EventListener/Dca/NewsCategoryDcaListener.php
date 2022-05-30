e8482a79ecd8ced30aab2ad6e0a57b811e733a0e
David Molineus <david.molineus@netzmacht.de>
2022-05-30 15:54:45 +0200

    Render category path for news categories

--- src/EventListener/Dca/NewsCategoryDcaListener.php
+++ src/EventListener/Dca/NewsCategoryDcaListener.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Hofff\Contao\ContactProfiles\EventListener\Dca;
 
+use Codefog\NewsCategoriesBundle\Model\NewsCategoryModel;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\CoreBundle\ServiceAnnotation\Callback;
 use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\DataContainer;
+use Contao\Input;
 use Hofff\Contao\ContactProfiles\Model\Profile\Profile;
+use Netzmacht\Contao\Toolkit\Data\Model\RepositoryManager;
 use Netzmacht\Contao\Toolkit\Dca\DcaManager;
+
+use function array_pop;
+use function array_unshift;
+use function implode;
 
 final class NewsCategoryDcaListener
 {
@@ -16,10 +24,13 @@ final class NewsCategoryDcaListener
 
     private array $bundles;
 
-    public function __construct(DcaManager $dcaManager, array $bundles)
+    private RepositoryManager $repositoryManager;
+
+    public function __construct(DcaManager $dcaManager, RepositoryManager $repositoryManager, array $bundles)
     {
-        $this->dcaManager = $dcaManager;
-        $this->bundles    = $bundles;
+        $this->dcaManager        = $dcaManager;
+        $this->bundles           = $bundles;
+        $this->repositoryManager = $repositoryManager;
     }
 
     /** @Callback(table="tl_news_category", target="config.onload") */
@@ -45,6 +56,42 @@ final class NewsCategoryDcaListener
 
                 return $fields;
             }
+        );
+    }
+
+    /** @Callback(table="tl_news_category", target="list.label.label") */
+    public function newsCategoryOptions(array $row, ?string $originalLabel, DataContainer $dataContainer): ?string
+    {
+        if (Input::get('do') !== 'hofff_contact_profiles') {
+            return $originalLabel;
+        }
+
+        $repository = $this->repositoryManager->getRepository(NewsCategoryModel::class);
+        $categories = [];
+        $label      = [];
+
+        $categoryId = $row['id'];
+
+        while ($categoryId > 0) {
+            $category = $categories[$categoryId] ?? $repository->find($categoryId);
+            if ($category === null) {
+                break;
+            }
+
+            $categoryId = $category->pid;
+
+            array_unshift($label, $category->getTitle());
+        }
+
+        $last = array_pop($label);
+        if ($label === []) {
+            return $last;
+        }
+
+        return sprintf(
+            '<span class="tl_gray">%s / </span> %s',
+            implode(' / ', $label),
+            $last
         );
     }
 }
