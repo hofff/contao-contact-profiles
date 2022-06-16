66a3ab69b87b5559d6661fe642553fa167697af8
David Molineus <david.molineus@netzmacht.de>
2022-06-16 14:13:16 +0200

    Fix palette initialization for different types

--- src/EventListener/Dca/InitializePalettesListener.php
+++ src/EventListener/Dca/InitializePalettesListener.php
@@ -7,9 +7,19 @@ namespace Hofff\Contao\ContactProfiles\EventListener\Dca;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\CoreBundle\ServiceAnnotation\Callback;
 use Contao\DataContainer;
+use Netzmacht\Contao\Toolkit\Dca\DcaManager;
+
+use function is_array;
 
 final class InitializePalettesListener
 {
+    private DcaManager $dcaManager;
+
+    public function __construct(DcaManager $dcaManager)
+    {
+        $this->dcaManager = $dcaManager;
+    }
+
     /**
      * @Callback(table="tl_calendar_events", target="config.onload")
      * @Callback(table="tl_faq", target="config.onload")
@@ -17,9 +27,21 @@ final class InitializePalettesListener
      */
     public function initializePalette(DataContainer $dataContainer): void
     {
-        PaletteManipulator::create()
-            ->addLegend('hofff_contact_profiles_legend', 'title_legend')
-            ->addField('hofff_contact_profiles', 'hofff_contact_profiles_legend', PaletteManipulator::POSITION_APPEND)
-            ->applyToPalette('default', $dataContainer->table);
+        $definition = $this->dcaManager->getDefinition($dataContainer->table);
+
+        foreach ($definition->get(['palettes'], []) as $palette => $config) {
+            if (is_array($config)) {
+                continue;
+            }
+
+            PaletteManipulator::create()
+                ->addLegend('hofff_contact_profiles_legend', 'title_legend')
+                ->addField(
+                    'hofff_contact_profiles',
+                    'hofff_contact_profiles_legend',
+                    PaletteManipulator::POSITION_APPEND
+                )
+                ->applyToPalette($palette, $dataContainer->table);
+        }
     }
 }
